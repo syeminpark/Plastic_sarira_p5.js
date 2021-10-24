d177b06f2ea4d3d828695d5f68f3f15b9c0376db
macbookPro <soccer0874@naver.com>
2021-10-24 13:56:09 +0900

    plus

--- PE.js
+++ PE.js
@@ -5,7 +5,7 @@ class PE extends Microplastic {
         // PE(polyethylene  PP(polypropylene) PS(polystyrene) PA(polyamide) PET(Polyester)  Acrylic  POM(polyoxymethylene) PVC(polyvinyl chloride) PU(polyurethane)
         this.type = "PE";
         this.dateCreated = 1979
-        this.originalPurposeList = ["Grocery Bag", "Squeeze Bottle", "Laundry Detergent", "Cutting Board", "Garbage Bin", "Sandwich Bag", "Frozen Ready-Meal Tray", "Cooking Oil Bottles", "First Aid Blankets", "Polar Fleece"]
+        this.originalPurposeList = ["Grocery Bag", "Squeeze Bottle", "Laundry Detergent", "Cutting Board", "Garbage Bin", "Sandwich Bag", "Ready-Meal Tray", "Cooking Oil Bottles", "First Aid Blankets", "Polar Fleece"]
         this.pastOwnersList;
         this.retrievedMethod;
         this.dateRetrieved;
